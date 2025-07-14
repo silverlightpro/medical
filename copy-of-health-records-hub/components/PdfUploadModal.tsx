01889cd22a973b2ce917045d1ee2a52523e49761
SilverlightPRO <161150464+silverlightpro@users.noreply.github.com>
2025-07-14 09:22:34 +0000

    delete

--- copy-of-health-records-hub/components/PdfUploadModal.tsx
+++
@@ -1,3 +0,0 @@
-// This file was replaced by RecordUploadModal.tsx and is no longer used.
-// It is populated with a valid module definition to prevent build errors.
-export {};
--- copy-of-health-records-hub/components/VitalFormModal.tsx
+++
@@ -1,3 +0,0 @@
-// This file is not currently used in the application.
-// It has been populated with a valid module definition to prevent syntax errors.
-export {};
